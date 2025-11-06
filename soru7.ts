7a7e6e88a9acb1635f63974e9ab23f44d99b878d
MkaanBB <zeromkb@gmail.com>
2025-11-06 22:45:32 +0300

    Create soru7.ts

---
+++ soru7.ts
@@ -0,0 +1,20 @@
+type User = { id: number; name: string; email: string };
+const users: User[] = [
+  { id: 1, name: "Ali", email: "ali@mail.com" },
+  { id: 2, name: "Ayşe", email: "ayse@mail.com" },
+];
+
+function updateUser(
+  id: number,
+  updates: Partial<User>
+): Readonly<User> | undefined {
+  const user = users.find((u) => u.id === id);
+  if (!user) {
+    return undefined;
+  }
+  return Object.assign(user, updates) as Readonly<User>;
+}
+
+console.log("Eski: ", users[0]);
+const updatedUser = updateUser(1, { email: "ali.yeni@mail.com" });
+console.log("Yeni: ", updatedUser);
